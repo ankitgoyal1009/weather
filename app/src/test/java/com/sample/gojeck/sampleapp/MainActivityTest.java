02b1d48ae55a8c365ce139668ffd9a6876a0ae39
Ankit <ankit.goyal@vymo.in>
2019-06-17 15:32:02 +0530

    some tet cases.

---
+++ app/src/test/java/com/sample/gojeck/sampleapp/MainActivityTest.java
@@ -0,0 +1,20 @@
+package com.sample.gojeck.sampleapp;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+    @Test
+    public void check_day_isCorrect() {
+        // This will check if day returned by dayForDate() is correct.
+    }
+
+    @Test
+    public void check_error_message_isCorrect() {
+        // This will check if error message returned by getErrorMessage() is correct
+    }
+
+    @Test
+    public void check_live_data_is_updating_state() {
+        // This method will call fetchWeatherData() function and checks if live data is changing the state from none->loading.
+    }
+}
